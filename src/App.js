31d5b98375bbd8dc317cf404a46bbbb7718583a6
Satinder Singh Saini <swv095@gmail.com>
2021-09-05 16:53:58 +0530

    Created Add and delete product functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
+import Products from './components/Products';
+import AddProduct from './components/AddProduct';
+import AdminProducts from './components/AdminProducts';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state={
+    products: []
+  }
+
+  addProduct = (product) =>{
+    const products = [...this.state.products];
+    product.id = Math.random();
+    
+    products.push(product);
+    this.setState({
+      products
+    });
+  }
+  deleteProduct = (id) =>{
+    const products = this.state.products.filter(p=>p.id!== id);
+    this.setState({
+      products
+    });
+  }
+  render() {
+    return (
+      <BrowserRouter>
+      
+      <div className="App">         
+            <Navbar />
+            <Switch>
+              <Route path="/add-product" render={ (props) => <AddProduct {...props} addProduct={this.addProduct}/> }></Route>
+              <Route path="/admin-products" render={ (props)=><AdminProducts {...props} deleteProduct = {this.deleteProduct} products={this.state.products}/>}></Route>
+              <Route path="/" component={Products}></Route>              
+            </Switch>           
+      </div>   
+               
+      </BrowserRouter>
+      
+    
+    );
+  }
 }
 
 export default App;
